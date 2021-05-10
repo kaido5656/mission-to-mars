c18b5aed26d7ec53bd8be58f8c7b4dd15e4bd02f
kaido5656 <78507488+kaido5656@users.noreply.github.com>
2021-05-09 19:48:11 -0500

    challenge commit

--- scraping.py
+++ scraping.py
@@ -18,7 +18,8 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        "last_modified": dt.datetime.now(),
+        "hemi": hemi(browser)  
     }
     # Stop webdriver and return data
     browser.quit()
@@ -103,7 +104,54 @@ def mars_facts():
     return df.to_html()
 
 
+def hemi(browser):
+
+    # 1. Use browser to visit the URL 
+    url = 'https://marshemispheres.com/'
+
+    browser.visit(url)
+    #hemi_html = browser.html
+    #hemi_soup = soup(hemi_html, 'html.parser')
+
+    # 2. Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+
+    # 3. Write code to retrieve the image urls and titles for each hemisphere.
+    #result = hemi_soup.find_all('div', class_='item')
+
+    for i in range(4):
+        hemispheres = {}
+        
+        #clicking on img link
+        full_image_elem = browser.find_by_tag('h3')[i]
+        full_image_elem.click()
+        
+        #parsing the html
+        hemi_html = browser.html
+        hemi_soup = soup(hemi_html, 'html.parser')
+        
+        
+        img_link = hemi_soup.find_all('div', class_='downloads')
+        for i in img_link:
+            x = i.find('li')
+            link = x.a['href']
+            img_url = (url + link)
+            
+        
+        title = hemi_soup.h2.text    
+
+
+        hemispheres = {'img_url': img_url, 'title': title}
+
+        hemisphere_image_urls.append(hemispheres)
+        
+        browser.back()
+
+    return hemisphere_image_urls
+
+
 if __name__ == "__main__":
 
     # If running as script, print scraped data
     print(scrape_all())
+
